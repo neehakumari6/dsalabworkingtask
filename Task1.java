d0091e0f8bef8524040fa37fe0f03e2aa459bf56
neehakumari6 <neehakumari336@gmail.com>
2024-10-23 23:40:50 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args){
+        int num = 10;
+        int sum = 0;
+        for(int i = 1; i <= 10; i++){
+            sum += i;
+        }
+        System.out.print(sum);
+    }
+}
+
